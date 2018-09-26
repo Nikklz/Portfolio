9e8c0d64d5ffa2046bce92e41d63ad69311f18bf
Nicole Zelingher <nicole.zelingher@gmail.com>
2018-09-26 13:16:29 -0400

    updated server port

--- app.js
+++ app.js
@@ -21,6 +21,6 @@ app.get("/projects", function(req,res){
 	res.render("projects");
 });
 
-app.listen(3000, process.env.IP, function(){
+app.listen(process.env.IP || 3000, function(){
     console.log("Server has started!");
 });
